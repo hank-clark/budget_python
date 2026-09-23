0193856aa7a26ba304275b6577ebbe28b17d3620
Hank <hank.clark.nc@gmail.com>
2026-09-23 10:22:44 -0400

    added update_cat_ql to allow for easier adding of subcategories and categories to transactions.db

---
+++ update_cat_sql.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import sqlite3
+
+sql_connection = sqlite3.connect('./data/processed/transactions.db')
+cursor = sql_connection.cursor()
+
+category_df = pd.read_excel('./data/processed/categories_and_subcategories.xlsx', sheet_name='categories')
+subcat_df = pd.read_excel('./data/processed/categories_and_subcategories.xlsx', sheet_name='subcategories')
+
+
+cursor.execute(
+    '''
+    CREATE TEMP TABLE cat_staging_table (
+        category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category_name TEXT
+    )
+'''
+)
+
+cursor.execute(
+    '''
+    CREATE TEMP TABLE subcat_staging_table (
+        subcategory_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        subcategory_name TEXT,
+        category_id INTEGER
+    )
+'''
+)
+
+category_df.to_sql('cat_staging_table', sql_connection, if_exists='append', index=False)
+subcat_df.to_sql('subcat_staging_table', sql_connection, if_exists='append', index=False)
+
+cursor.execute(
+    '''
+    INSERT OR IGNORE INTO categories (category_id, category_name)
+    SELECT category_id, category_name
+    FROM cat_staging_table
+'''
+)
+
+cursor.execute(
+    '''
+    INSERT OR IGNORE INTO subcategories (subcategory_id, subcategory_name, category_id)
+    SELECT subcategory_id, subcategory_name, category_id
+    FROM subcat_staging_table
+'''
+)
+
+sql_connection.commit()
+sql_connection.close()
